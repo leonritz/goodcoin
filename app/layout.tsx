1ca023e859cd15d2e4fa740224785ffb4f221955
Your Name <you@example.com>
2025-10-23 21:34:26 +0200

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   return (
     <RootProvider>
       <html lang="en">
-        <body className={`${inter.variable} ${sourceCodePro.variable}`}>
+        <body className={`${inter.variable} ${sourceCodePro.variable}`} suppressHydrationWarning={true}>
           <SafeArea>{children}</SafeArea>
         </body>
       </html>
